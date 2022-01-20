6e46b5d5a05418de4c4b0804ca097d4de32eea40
Udith Shyamsukha <udithshyamsukha@gmail.com>
2022-01-20 16:36:22 +0530

    Restructured Code

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const ejsMate = require('ejs-mate')
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
@@ -10,6 +11,7 @@ mongoose.connect('mongodb://localhost:27017/Revster')
     .then(() => console.log('Connected to database'))
     .catch((e) => console.log("Error", e))
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
