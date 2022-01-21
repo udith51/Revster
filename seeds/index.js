530acdbd02a8c0ea3243c3dffd63a016932cb8b4
Udith Shyamsukha <udithshyamsukha@gmail.com>
2022-01-21 18:11:08 +0530

    Basic UI updation

--- seeds/index.js
+++ seeds/index.js
@@ -10,6 +10,8 @@ mongoose.connect('mongodb://localhost:27017/Revster')
 
 const sample = arr => arr[Math.floor(Math.random() * arr.length)]
 
+const getPrice = () => { return ((Math.floor(Math.random() * 100) * 100) + 1000) }
+
 const getImage = async () => {
     try {
         const data = await axios.get('https://api.unsplash.com/photos/random?client_id=1P06z6ltWTyFPfcUOVv1Pbc6e0imnSiXmmcYBCFsd_c&collections=2184453');
@@ -24,12 +26,13 @@ const getImage = async () => {
 
 const seedDb = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 15; i++) {
+    for (let i = 0; i < 50; i++) {
         const random = Math.floor(Math.random() * 1000);
         const camp = new Campground({
             location: `${cities[random].city}-${cities[random].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: await getImage(),
+            price: getPrice(),
             description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio aliquid aut, quibusdam facere doloribus atque sapiente possimus fugit nemo ex corrupti ab non nisi rerum fugiat maxime quisquam blanditiis quam.",
         })
         await camp.save();
