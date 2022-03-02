f47b632dea2c8af53d7686e163d5d6c7cc107f8c
Udith Shyamsukha <udithshyamsukha@gmail.com>
2022-03-03 01:26:09 +0530

    Auth controller file added

---
+++ controllers/auth_controller.js
@@ -0,0 +1,50 @@
+const User = require('../models/user');
+const passport = require('passport');
+
+module.exports.registrationForm = (req, res) => {
+    res.render('auth/register');
+}
+
+module.exports.registerUser = async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        const newUser = new User({ email, username });
+        const regUser = await User.register(newUser, password);
+        req.login(regUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Welcome to Yelpcamp!');
+            res.redirect('/campgrounds');
+        })
+    } catch (e) {
+        if (e.code == 11000) {
+            req.flash('error', ' A user with the given email id is already registered');
+            res.redirect('/register');
+        }
+        else {
+            req.flash('error', e.message);
+            res.redirect('/register');
+        }
+    }
+}
+
+module.exports.loginForm = (req, res) => {
+    res.render('auth/login');
+}
+
+module.exports.loginUser = (req, res) => {
+    req.flash('success', 'Welcome back!');
+    if (req.session.returnTo) {
+        const add = req.session.returnTo;
+        delete req.session.returnTo;
+        res.redirect(add);
+    }
+    else {
+        res.redirect('/campgrounds');
+    }
+}
+
+module.exports.logoutUser = (req, res) => {
+    req.logout();
+    req.flash('success', "Goodbye!");
+    res.redirect('/campgrounds');
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,53 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const users_controller = require('../controllers/auth_controller');
 const User = require('../models/user');
 const catchAsync = require('../utilities/catchAsync');
 
 
-router.get('/register', (req, res) => {
-    res.render('auth/register');
-})
-router.post('/register', catchAsync(async (req, res) => {
-    try {
-        const { email, username, password } = req.body;
-        const newUser = new User({ email, username });
-        const regUser = await User.register(newUser, password);
-        req.login(regUser, err => {
-            if (err) return next(err);
-            req.flash('success', 'Welcome to Yelpcamp!');
-            res.redirect('/campgrounds');
-        })
-    } catch (e) {
-        if (e.code == 11000) {
-            req.flash('error', ' A user with the given email id is already registered');
-            res.redirect('/register');
-        }
-        else {
-            req.flash('error', e.message);
-            res.redirect('/register');
-        }
-    }
-}))
-router.get('/login', (req, res) => {
-    res.render('auth/login');
-})
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-    req.flash('success', 'Welcome back!');
-    if (req.session.returnTo) {
-        const add = req.session.returnTo;
-        delete req.session.returnTo;
-        res.redirect(add);
-    }
-    else {
-        res.redirect('/campgrounds');
-    }
+router.get('/register', users_controller.registrationForm);
 
-})
-router.get('/logout', (req, res) => {
-    req.logout();
-    req.flash('success', "Goodbye!");
-    res.redirect('/campgrounds');
-})
+router.post('/register', catchAsync(users_controller.registerUser));
+
+router.get('/login', users_controller.loginForm);
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users_controller.loginUser);
+
+router.get('/logout', users_controller.logoutUser);
 
 module.exports = router;
