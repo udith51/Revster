5e0ad9de0224dcafd153c797b17fe1488f1fab88
Udith Shyamsukha <udithshyamsukha@gmail.com>
2023-05-26 21:48:55 +0530

    Updated deprecated db

--- seeds/index.js
+++ seeds/index.js
@@ -4,7 +4,7 @@ const Campground = require('../models/campground');
 const cities = require('./cities');
 const { descriptors, places } = require('./seedhelpers');
 
-mongoose.connect('mongodb://localhost:27017/Revster')
+mongoose.connect('mongodb+srv://udith28:WomQd6Vxs8BbBxhj@cluster0.byuq9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
     .then(() => console.log('Connected to database'))
     .catch((e) => console.log("Error", e))
 
@@ -29,7 +29,7 @@ const seedDb = async () => {
         const random = Math.floor(Math.random() * 1000);
         const camp = new Campground({
             location: `${cities[random].city}-${cities[random].state}`,
-            author: '6214a378560928f39b1b367a',
+            author: '6232e35916bc97b4e178c721',
             title: `${sample(descriptors)} ${sample(places)}`,
             images: [
                 {
